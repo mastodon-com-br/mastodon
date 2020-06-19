e18d908b2b304216e08e7bfed0f2702cc5bd6b5f
Claus Wahlers <claus@codeazur.com.br>
2020-06-18 23:25:30 -0300

    Add `/about/migrar` page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -6,9 +6,9 @@ class AboutController < ApplicationController
   before_action :require_open_federation!, only: [:show, :more]
   before_action :set_body_classes, only: :show
   before_action :set_instance_presenter
-  before_action :set_expires_in, only: [:show, :more, :terms]
+  before_action :set_expires_in, only: [:show, :more, :terms, :migrar]
 
-  skip_before_action :require_functional!, only: [:more, :terms]
+  skip_before_action :require_functional!, only: [:more, :terms, :migrar]
 
   def show; end
 
@@ -23,6 +23,8 @@ class AboutController < ApplicationController
   end
 
   def terms; end
+
+  def migrar; end
 
   helper_method :display_blocks?
   helper_method :display_blocks_rationale?
